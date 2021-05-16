9eeda00d69f8b47ce0a7ebc81e4fc3f8d7ef0877
UMASHIBA <umashiba1@gmail.com>
2021-05-16 18:03:33 +0900

    mod: change way to measure performance of assembly-fib

--- app/fib/assemblyscript-fib/index.js
+++ app/fib/assemblyscript-fib/index.js
@@ -7,15 +7,32 @@ async function sample() {
 }
 
 sample().then((wasmModule) => {
-    for (let i = 10; i <= 50; i += 10) {
+
+    // fib 10~50を計算する調査
+    // for (let i = 10; i <= 50; i += 10) {
+    //     console.group();
+    //     console.log(`n: ${i}`)
+    //     const startFib = performance.now();
+    //     console.log(`start fib ${startFib}`);
+    //     console.log(`fib result ${wasmModule.exports.fib(BigInt(i))}`);
+    //     const endFib = performance.now();
+    //     console.log(`end fib ${endFib}`);
+    //     console.log(`processing time: ${endFib - startFib}ms`);
+    //     console.groupEnd();
+    // };
+
+    // fib 40を複数回計算する調査
+    const processing_times = [];
+    for (let i = 0; i < 10; i++) {
         console.group();
-        console.log(`n: ${i}`)
         const startFib = performance.now();
-        console.log(`start fib ${startFib}`);
-        console.log(`fib result ${wasmModule.exports.fib(BigInt(i))}`);
+        console.log("fib result", wasmModule.exports.fib(BigInt(40)));
         const endFib = performance.now();
-        console.log(`end fib ${endFib}`);
-        console.log(`processing time: ${endFib - startFib}ms`);
+        const proc_time = endFib - startFib;
+        console.log(`processing time: ${proc_time}ms`);
+        processing_times.push(proc_time);
         console.groupEnd();
-    };
+    }
+    console.log(processing_times);
+
 });
