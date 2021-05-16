9c8aa60225e162dc6f2d2de7b9a1177397287a9b
UMASHIBA <umashiba1@gmail.com>
2021-05-16 17:06:41 +0900

    mod: change way to measure performance

--- app/fib/rust-fib/www/index.js
+++ app/fib/rust-fib/www/index.js
@@ -1,13 +1,29 @@
 import("../pkg").then(module => {
-    for(let i = 10; i <= 40; i+=10) {
+
+    // fib 10~50を計算する調査
+    // for(let i = 10; i <= 40; i+=10) {
+    //     console.group();
+    //     console.log(`n: ${i}`);
+    //     const startFib = performance.now();
+    //     console.log(`start fib: `, startFib);
+    //     console.log(`fib result: ${module.fib(BigInt(i))}`);
+    //     const endFib = performance.now();
+    //     console.log(`end fib: ${endFib}`);
+    //     console.log(`processing time: ${endFib - startFib}ms`);
+    //     console.groupEnd();
+    // }
+
+    // fib 40を複数回計算する調査
+    const processing_times = [];
+    for(let i = 0; i < 10; i++) {
         console.group();
-        console.log(`n: ${i}`);
         const startFib = performance.now();
-        console.log(`start fib: `, startFib);
-        console.log(`fib result: ${module.fib(BigInt(i))}`);
+        console.log(`fib result: ${module.fib(BigInt(40))}`);
         const endFib = performance.now();
-        console.log(`end fib: ${endFib}`);
+        const proc_time = endFib - startFib;
         console.log(`processing time: ${endFib - startFib}ms`);
+        processing_times.push(proc_time);
         console.groupEnd();
     }
+    console.log(processing_times);
 })
