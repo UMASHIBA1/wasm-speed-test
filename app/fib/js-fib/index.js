6f40e81470ce0c8a164f627166757cdff4fefb20
UMASHIBA <umashiba1@gmail.com>
2021-05-17 17:57:08 +0900

    mod: change js-fib

--- app/fib/js-fib/index.js
+++ app/fib/js-fib/index.js
@@ -7,9 +7,20 @@ const fib = function (n) {
   return fib(n - 1) + fib(n - 2);
 };
 
-console.group();
-const startFib = performance.now();
-console.log("fib result", fib(40));
-const endFib = performance.now();
-console.log(`processing time: ${endFib - startFib}ms`);
-console.groupEnd();
+// 最適化をかけるために一回実行しとく
+fib(40);
+
+const processing_times = [];
+
+for (let i = 0; i < 10; i++) {
+  console.group();
+  const startFib = performance.now();
+  console.log("fib result", fib(40));
+  const endFib = performance.now();
+  const processing_time = endFib - startFib;
+  console.log(`processing time: ${processing_time}ms`);
+  processing_times.push(processing_time);
+  console.groupEnd();
+}
+
+console.log(processing_times);
